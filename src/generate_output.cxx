40cd25ba2ee284da289a139410cec423aff256aa
nick-elsey <nicholas.elsey@gmail.com>
2016-10-05 13:52:36 -0400

    further testing -

--- src/generate_output.cxx
+++ src/generate_output.cxx
@@ -292,6 +292,7 @@ int main( int argc, const char** argv) {
       std::string corrName = "tmp/corrtest_";  corrName += patch::to_string(i); corrName += patch::to_string(j); corrName += ".pdf";
       std::string mixName = "tmp/mixtest_"; mixName += patch::to_string(i); mixName += patch::to_string(j); mixName += ".pdf";
       std::string corrNameProj = "tmp/corrtestproj_"; corrNameProj += patch::to_string(i); corrNameProj += patch::to_string(j); corrNameProj += ".pdf";
+      td::string corrNameProj2 = "tmp/corrtestproj_"; corrNameProj2 += patch::to_string(i); corrNameProj2 += patch::to_string(j); corrNameProj2 += ".pdf";
       
       reducedHist[i][j]->Draw("surf1");
       c1.SaveAs( corrName.c_str() );
@@ -299,6 +300,8 @@ int main( int argc, const char** argv) {
       c1.SaveAs( mixName.c_str() );
       reducedHist[i][j]->ProjectionY()->Draw();
       c1.SaveAs( corrNameProj.c_str() );
+      reducedHist[i][j]->ProjectionX()->Draw();
+      c1.SaveAs( corrNameProj2.c_str() );
       
     }
   }
