06943e72157df2fee1d3c160934fabd300ac662f
nick-elsey <nicholas.elsey@gmail.com>
2016-10-14 20:59:15 -0400

    testing tree loading

--- src/event_mixing.cxx
+++ src/event_mixing.cxx
@@ -381,9 +381,9 @@ int main ( int argc, const char** argv) {
   
   // set the branch addresses for the tree
   if ( requireDijets ) {
-    jetTree->SetBranchAddress( "leadJet", &leadBranch );
+    //jetTree->SetBranchAddress( "leadJet", &leadBranch );
     //jetTree->SetBranchAddress( "subLeadJet", &subBranch );
-    //jetTree->SetBranchAddress( "vertexZBin", &vzBranch );
+    jetTree->SetBranchAddress( "vertexZBin", &vzBranch );
     //if ( analysisType == "dijetmix" )
       //jetTree->SetBranchAddress( "centralityBin", &centBranch );
   }
