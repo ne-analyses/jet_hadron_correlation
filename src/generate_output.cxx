a71516b4087317cf23e8c2d97bd34c0ef581b6ca
nick-elsey <nicholas.elsey@gmail.com>
2016-10-31 22:28:54 -0400

    fixed namespace

--- src/generate_output.cxx
+++ src/generate_output.cxx
@@ -88,7 +88,6 @@ int main( int argc, const char** argv) {
   double ptBinWidth[nPtBins];
   for ( int i = 0; i < nPtBins; ++i ) {
     ptBinWidth[i] = ( ptBinHi[i] - ptBinLo[i] + 1 ) * 0.25;
-    std::cout<<"bin: "<<i<<": "<<ptBinWidth[i]<<std::endl;
   }
   
   // analysis names
@@ -876,17 +875,17 @@ int main( int argc, const char** argv) {
     subEtaYield[i].resize( nPtBins );
     subEtaError[i].resize( nPtBins );
     for ( int j = 0; j < nPtBins; ++j ) {
-      leadPhiYield[i][j] = leadPhiFit[i][j]->GetParameter(1)*sqrt(2*corrAnalysis::pi)*leadPhiFit[i][j]->GetParameter(3);
+      leadPhiYield[i][j] = leadPhiFit[i][j]->GetParameter(1)*sqrt(2*corrAnalysis::pi)*leadPhiFit[i][j]->GetParameter(3)/ptBinWidth[j];
       leadPhiError[i][j] = leadPhiFit[i][j]->GetParError(1);
-      leadPhiDifYield[i][j] = leadPhiDifFit[i][j]->GetParameter(1)*sqrt(2*corrAnalysis::pi)*leadPhiDifFit[i][j]->GetParameter(3);
+      leadPhiDifYield[i][j] = leadPhiDifFit[i][j]->GetParameter(1)*sqrt(2*corrAnalysis::pi)*leadPhiDifFit[i][j]->GetParameter(3)/ptBinWidth[j];
       leadPhiDifError[i][j] = leadPhiDifFit[i][j]->GetParError(1);
-      leadEtaYield[i][j] = leadEtaFit[i][j]->GetParameter(1)*sqrt(2*corrAnalysis::pi)*leadEtaFit[i][j]->GetParameter(3);
+      leadEtaYield[i][j] = leadEtaFit[i][j]->GetParameter(1)*sqrt(2*corrAnalysis::pi)*leadEtaFit[i][j]->GetParameter(3)/ptBinWidth[j];
       leadEtaError[i][j] = leadEtaFit[i][j]->GetParError(1);
-      subPhiYield[i][j] = subPhiFit[i][j]->GetParameter(1)*sqrt(2*corrAnalysis::pi)*subPhiFit[i][j]->GetParameter(3);
+      subPhiYield[i][j] = subPhiFit[i][j]->GetParameter(1)*sqrt(2*corrAnalysis::pi)*subPhiFit[i][j]->GetParameter(3)/ptBinWidth[j];
       subPhiError[i][j] = subPhiFit[i][j]->GetParError(1);
-      subPhiDifYield[i][j] = subPhiDifFit[i][j]->GetParameter(1)*sqrt(2*corrAnalysis::pi)*subPhiDifFit[i][j]->GetParameter(3);
+      subPhiDifYield[i][j] = subPhiDifFit[i][j]->GetParameter(1)*sqrt(2*corrAnalysis::pi)*subPhiDifFit[i][j]->GetParameter(3)/ptBinWidth[j];
       subPhiDifError[i][j] = subPhiDifFit[i][j]->GetParError(1);
-      subEtaYield[i][j] = subEtaFit[i][j]->GetParameter(1)*sqrt(2*corrAnalysis::pi)*subEtaFit[i][j]->GetParameter(3);
+      subEtaYield[i][j] = subEtaFit[i][j]->GetParameter(1)*sqrt(2*corrAnalysis::pi)*subEtaFit[i][j]->GetParameter(3)/ptBinWidth[j];
       subEtaError[i][j] = subEtaFit[i][j]->GetParError(1);
     }
   }
--- src/generate_output_avg.cxx
+++ src/generate_output_avg.cxx
@@ -87,7 +87,7 @@ int main( int argc, const char** argv) {
   std::string ptBinString[nPtBins] = { "0.5 < p_{T} < 1.0", "1.0 < p_{T} < 2.0", "2.0 < p_{T} < 3.0", "3.0 < p_{T} < 4.0", "4.0 < p_{T} < 6.0" };
   double ptBinWidth[nPtBins];
   for ( int i = 0; i < nPtBins; ++i ) {
-    ptBinWidth[i] = ( ptBinHi[i] - ptBinLo[i] ) * 0.25;
+    ptBinWidth[i] = ( ptBinHi[i] - ptBinLo[i] + 1 ) * 0.25;
   }
   
   // analysis names
@@ -903,17 +903,17 @@ int main( int argc, const char** argv) {
     subEtaYield[i].resize( nPtBins );
     subEtaError[i].resize( nPtBins );
     for ( int j = 0; j < nPtBins; ++j ) {
-      leadPhiYield[i][j] = leadPhiFit[i][j]->GetParameter(1)*sqrt(2*corrAnalysis::pi)*leadPhiFit[i][j]->GetParameter(3);
+      leadPhiYield[i][j] = leadPhiFit[i][j]->GetParameter(1)*sqrt(2*corrAnalysis::pi)*leadPhiFit[i][j]->GetParameter(3)/ptBinWidth[j];
       leadPhiError[i][j] = leadPhiFit[i][j]->GetParError(1);
-      leadPhiDifYield[i][j] = leadPhiDifFit[i][j]->GetParameter(1)*sqrt(2*corrAnalysis::pi)*leadPhiDifFit[i][j]->GetParameter(3);
+      leadPhiDifYield[i][j] = leadPhiDifFit[i][j]->GetParameter(1)*sqrt(2*corrAnalysis::pi)*leadPhiDifFit[i][j]->GetParameter(3)/ptBinWidth[j];
       leadPhiDifError[i][j] = leadPhiDifFit[i][j]->GetParError(1);
-      leadEtaYield[i][j] = leadEtaFit[i][j]->GetParameter(1)*sqrt(2*corrAnalysis::pi)*leadEtaFit[i][j]->GetParameter(3);
+      leadEtaYield[i][j] = leadEtaFit[i][j]->GetParameter(1)*sqrt(2*corrAnalysis::pi)*leadEtaFit[i][j]->GetParameter(3)/ptBinWidth[j];
       leadEtaError[i][j] = leadEtaFit[i][j]->GetParError(1);
-      subPhiYield[i][j] = subPhiFit[i][j]->GetParameter(1)*sqrt(2*corrAnalysis::pi)*subPhiFit[i][j]->GetParameter(3);
+      subPhiYield[i][j] = subPhiFit[i][j]->GetParameter(1)*sqrt(2*corrAnalysis::pi)*subPhiFit[i][j]->GetParameter(3)/ptBinWidth[j];
       subPhiError[i][j] = subPhiFit[i][j]->GetParError(1);
-      subPhiDifYield[i][j] = subPhiDifFit[i][j]->GetParameter(1)*sqrt(2*corrAnalysis::pi)*subPhiDifFit[i][j]->GetParameter(3);
+      subPhiDifYield[i][j] = subPhiDifFit[i][j]->GetParameter(1)*sqrt(2*corrAnalysis::pi)*subPhiDifFit[i][j]->GetParameter(3)/ptBinWidth[j];
       subPhiDifError[i][j] = subPhiDifFit[i][j]->GetParError(1);
-      subEtaYield[i][j] = subEtaFit[i][j]->GetParameter(1)*sqrt(2*corrAnalysis::pi)*subEtaFit[i][j]->GetParameter(3);
+      subEtaYield[i][j] = subEtaFit[i][j]->GetParameter(1)*sqrt(2*corrAnalysis::pi)*subEtaFit[i][j]->GetParameter(3/ptBinWidth[j]);
       subEtaError[i][j] = subEtaFit[i][j]->GetParError(1);
 
     }
