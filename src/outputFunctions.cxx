10c436f1e87918ca9604d19f8e947e78890afc76
nick-elsey <nicholas.elsey@gmail.com>
2016-12-11 18:52:43 -0500

    starting to build new output routines

---
+++ src/outputFunctions.cxx
@@ -0,0 +1,15 @@
+// Implementation of basic functions
+// used in the output workflow of
+// the jet hadron correlation analysis
+
+#include "outputFunctions.hh"
+#include "corrParameters.hh"
+#include "histograms.hh"
+
+
+namespace jetHadron {
+  
+  
+  
+  
+} // end namespace
---
+++ src/outputFunctions.hh
@@ -0,0 +1,59 @@
+// Basic functions used in the
+// jet-hadron correlation output
+// workflow histogram adding/dividing,
+// drawing, etc
+
+// Nick Elsey
+
+// Make use of std::vector,
+// std::string, IO and algorithm
+// STL Headers
+#include <stdio.h>
+#include <stdlib.h>
+#include <sstream>
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <string>
+#include <limits.h>
+#include <unistd.h>
+
+// ROOT is used for histograms and
+// As a base for the TStarJetPico library
+// ROOT Headers
+#include "TH1.h"
+#include "TH2.h"
+#include "TH3.h"
+#include "TF1.h"
+#include "TF2.h"
+#include "TProfile.h"
+#include "TProfile2D.h"
+#include "TObjArray.h"
+#include "TString.h"
+#include "TFile.h"
+#include "TLorentzVector.h"
+#include "TClonesArray.h"
+#include "TChain.h"
+#include "TBranch.h"
+#include "TMath.h"
+#include "TRandom.h"
+#include "TRandom3.h"
+#include "TCanvas.h"
+#include "TStopwatch.h"
+#include "TSystem.h"
+
+#ifndef OUTPUTFUNCTIONS_HH
+#define OUTPUTFUNCTIONS_HH
+
+namespace jetHadron {
+  
+  
+  
+  
+  
+} // end namespace
+
+#endif
