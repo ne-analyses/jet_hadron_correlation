c8f94f4595b9aa3d55edddf63c0e5d34449f17e3
nick-elsey <nicholas.elsey@gmail.com>
2016-12-12 14:09:06 -0500

    testing

--- src/outputFunctions.cxx
+++ src/outputFunctions.cxx
@@ -60,7 +60,7 @@ namespace jetHadron {
             std::string leadName = "lead_aj_" + patch::to_string(j) + "_cent_" + patch::to_string(k) + "_vz_" + patch::to_string(l);
             std::string subLeadName = "sub_aj_" + patch::to_string(j) + "_cent_" + patch::to_string(k) + "_vz_" + patch::to_string(l);
             std::cout<<"getting histograms"<<std::endl;
-            TH3F* testing = new TH3F("testing","testing", 1, 0, 1, 1, 0, 1, 1, 0, 1 )
+            TH3F* testing = new TH3F("testing","testing", 1, 0, 1, 1, 0, 1, 1, 0, 1 );
             leadingCorrelations[i][j][k].push_back( testing );
             // get the correlation histograms
             //leadingCorrelations[i][j][k].push_back( (TH3F*) filesIn[i]->Get( leadName.c_str() ) );
