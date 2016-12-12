119f1ebc7fc18fe19a56715ec7dc64b7cecafef5
nick-elsey <nicholas.elsey@gmail.com>
2016-12-12 13:47:03 -0500

    testing outputFunctions

--- src/test.cxx
+++ src/test.cxx
@@ -2,6 +2,7 @@
 
 #include "corrParameters.hh"
 #include "corrFunctions.hh"
+#include "outputFunctions.hh"
 
 #include <cmath>
 #include <iostream>
@@ -46,29 +47,18 @@
 
 int main() {
   
-  TH1D* test = new TH1D("test", "Effective Acceptance", 500, -2, 2);
+  // test read in
+  std::vector<TFile> inFile;
+  inFile.push_back( TFile("out/tmp/dijet_20_10.root") );
   
-  // set up generator
-  std::random_device rd1;
-  std::mt19937 gen1(rd1());
-  std::uniform_real_distribution<> random1( -0.6, 0.6 );
+  // testing
+  jetHadron::binSelector selector;
   
-  // set up generator
-  std::random_device rd2;
-  std::mt19937 gen2(rd2());
-  std::uniform_real_distribution<> random2( -1, 1 );
+  std::vector<TH3F*> nEvents;
+  std::vector<std::vector<std::vector<std::vector<TH3F*> > > > leading;
+  std::vector<std::vector<std::vector<std::vector<TH3F*> > > > sub;
   
+  jetHadron::ReadInFiles( inFile, leading, sub, selector );
   
-  for ( int i = 0; i < 10000; ++i ) {
-    double trigger = random1(gen1);
-    for ( int j = 0; j < 10000; ++j ) {
-      double assoc = random2(gen1);
-      test->Fill( trigger - assoc );
-    }
-  }
-  
-  TCanvas c1;
-  test->Draw();
-  c1.SaveAs("test.pdf");
 	return 0;
 }
