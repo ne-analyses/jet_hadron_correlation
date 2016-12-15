edf07811683cb0e912a7431e34ca033dfb9ba376
nick-elsey <nicholas.elsey@gmail.com>
2016-12-15 14:38:52 -0500

    testing

--- src/outputFunctions.cxx
+++ src/outputFunctions.cxx
@@ -37,7 +37,7 @@ namespace jetHadron {
   
   // setting ranges for the histogram
   // if the radius is NOT 0.4, this needs to be used...
-  void binSelector::ChangeRadius( double R = 0.4) {
+  void binSelector::ChangeRadius( double R ) {
     dEtaAcceptanceLow = R - 2.0;
     dEtaAcceptanceHigh = 2.0 - R;
   }
