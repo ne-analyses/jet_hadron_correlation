cc0db2d6d4938721752212bcea50f287329781ee
nick-elsey <nicholas.elsey@gmail.com>
2017-01-06 01:33:43 -0500

    testing

--- src/generate_output.cxx
+++ src/generate_output.cxx
@@ -110,20 +110,20 @@ int main( int argc, const char** argv) {
       __OUT( "Using Default Settings" )
       
       // default files
-      TFile* tmp = new TFile( "out/tmp/auau_corr_6.root", "READ" );
-      TFile* tmpMix = new TFile( "out/tmp/auau_mix_6.root", "READ" );
+      TFile* tmp = new TFile( "out/tmp/auau_corr_7.root", "READ" );
+      TFile* tmpMix = new TFile( "out/tmp/auau_mix_7.root", "READ" );
       corrFiles.push_back( tmp );
       mixFiles.push_back( tmpMix );
       
-      tmp = new TFile( "out/tmp/pp_corr_6.root", "READ" );
-      tmpMix = new TFile( "out/tmp/pp_mix_6.root", "READ");
+      tmp = new TFile( "out/tmp/pp_corr_7.root", "READ" );
+      tmpMix = new TFile( "out/tmp/pp_mix_7.root", "READ");
       
       corrFiles.push_back( tmp );
       mixFiles.push_back( tmpMix );
 
       ajSplitBin = 5;
       analysisNames = defaultCorrNames;
-      outputDirBase = "/results/jet_20_10_trig_6";
+      outputDirBase = "/results/jet_20_10_trig_7";
       
       break;
     }
