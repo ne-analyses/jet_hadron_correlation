40bba0eaa8215e6f5c9ae88c72fc5726cc3425ec
nick-elsey <nicholas.elsey@gmail.com>
2017-01-06 00:46:05 -0500

    fixed spelling mismatches

--- src/generate_output.cxx
+++ src/generate_output.cxx
@@ -75,6 +75,10 @@ int main( int argc, const char** argv) {
   
   gStyle->SetOptStat(false);
   gStyle->SetOptFit(false);
+  
+  Float_t lmargin2d=0.20;
+  gPad->SetLeftMargin  ( lmargin2d );
+  gPad->SetBottomMargin( lmargin2d ); // standard is usually okay
 
   
   // First check to make sure we're located properly
