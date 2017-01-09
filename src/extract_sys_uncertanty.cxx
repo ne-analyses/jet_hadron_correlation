71cbba67cae596f215b4bd62c341ce734a18d32b
nick-elsey <nicholas.elsey@gmail.com>
2017-01-09 00:23:11 -0500

    setup skeleton for extraction of systematic uncertainties

---
+++ src/extract_sys_uncertanty.cxx
@@ -0,0 +1,67 @@
+// macro to extract systematic uncertainties
+// for pp correlations wrt auau
+// Nick Elsey 10/04/2016
+
+// All reader and histogram settings
+// Are located in corrParameters.hh
+#include "corrParameters.hh"
+
+// The majority of the jetfinding
+// And correlation code is located in
+// corrFunctions.hh
+#include "corrFunctions.hh"
+
+// The output functionality is
+// located here
+#include "outputFunctions.hh"
+
+// ROOT is used for histograms and
+// As a base for the TStarJetPico library
+// ROOT Headers
+#include "TH1.h"
+#include "TH2.h"
+#include "TH3.h"
+#include "TF1.h"
+#include "TF2.h"
+#include "TProfile.h"
+#include "TProfile2D.h"
+#include "TGraph.h"
+#include "TGraphErrors.h"
+#include "TLegend.h"
+#include "TObjArray.h"
+#include "TString.h"
+#include "TFile.h"
+#include "TLorentzVector.h"
+#include "TClonesArray.h"
+#include "TChain.h"
+#include "TBranch.h"
+#include "TMath.h"
+#include "TRandom.h"
+#include "TRandom3.h"
+#include "TCanvas.h"
+#include "TStopwatch.h"
+#include "TSystem.h"
+#include "TStyle.h"
+
+// Make use of std::vector,
+// std::string, IO and algorithm
+// STL Headers
+#include <stdio.h>
+#include <stdlib.h>
+#include <sstream>
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <string>
+#include <limits.h>
+#include <unistd.h>
+
+
+int main () {
+  
+  
+  return 0;
+}
