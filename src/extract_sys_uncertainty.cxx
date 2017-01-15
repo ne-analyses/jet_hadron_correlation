01b5bd35750ffb392baee90d1f703aecd318a4ca
nick-elsey <nicholas.elsey@gmail.com>
2017-01-14 22:59:03 -0500

    testing

--- src/extract_sys_uncertainty.cxx
+++ src/extract_sys_uncertainty.cxx
@@ -284,16 +284,6 @@ int main () {
   
   
   
-  
-  // output file
-  TString outPath = path + "/sys.root";
-  TFile* out = new TFile( outPath,"RECREATE");
-  
-  for ( int i = 0; i < corrected_deta_tow_fit.size(); ++i ) {
-    for ( int j = 0; j < corrected_deta_tow_fit[i].size(); ++j ) {
-      corrected_deta_tow[i][j]->Write();
-    }
-  }
-  out->Close();
+
   return 0;
 }
