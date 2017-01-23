e9de0d28df013b2f9b6a7b9d7a0731ea772b87da
nick-elsey <nicholas.elsey@gmail.com>
2017-01-22 22:50:13 -0500

    testing

--- src/outputFunctions.cxx
+++ src/outputFunctions.cxx
@@ -2705,7 +2705,7 @@ namespace jetHadron {
         errors2[i]->SetFillStyle(1001);
         errors2[i]->SetLineWidth(0);
         errors2[i]->SetMarkerSize(0);
-        errors2[i]->SetFillColorAlpha( kRed+4, 0.20 );
+        errors2[i]->SetFillColorAlpha( kMagenta+3, 0.20 );
         errors2[i]->GetXaxis()->SetRangeUser( rangeLow, rangeHigh );
         errors2[i]->GetYaxis()->SetRangeUser( min, max );
         
@@ -2798,7 +2798,7 @@ namespace jetHadron {
         errors2[i]->SetFillStyle(1001);
         errors2[i]->SetLineWidth(0);
         errors2[i]->SetMarkerSize(0);
-        errors2[i]->SetFillColorAlpha( kRed+4, 0.20 );
+        errors2[i]->SetFillColorAlpha( kkMagenta+3, 0.20 );
         errors2[i]->GetXaxis()->SetRangeUser( rangeLow, rangeHigh );
         errors2[i]->GetYaxis()->SetRangeUser( min, max );
         
@@ -2888,7 +2888,7 @@ namespace jetHadron {
       
       if ( i == 0 ) {
         sys2[0]->SetFillStyle(1001);
-        sys2[0]->SetFillColorAlpha( kRed+4, 0.20 );
+        sys2[0]->SetFillColorAlpha( 46, 0.20 );
       }
       
     }
