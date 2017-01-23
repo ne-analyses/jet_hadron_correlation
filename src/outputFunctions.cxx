b7b3e2e702d1f780c94179581447057f9a6a846d
nick-elsey <nicholas.elsey@gmail.com>
2017-01-22 20:57:12 -0500

    testing

--- src/outputFunctions.cxx
+++ src/outputFunctions.cxx
@@ -2705,7 +2705,7 @@ namespace jetHadron {
         errors2[i]->SetFillStyle(1001);
         errors2[i]->SetLineWidth(0);
         errors2[i]->SetMarkerSize(0);
-        errors2[i]->SetFillColorAlpha( kRed+4, 0.40 );
+        errors2[i]->SetFillColorAlpha( kCyan, 0.20 );
         
       }
       
@@ -2795,7 +2795,7 @@ namespace jetHadron {
         errors2[i]->SetFillStyle(1001);
         errors2[i]->SetLineWidth(0);
         errors2[i]->SetMarkerSize(0);
-        errors2[i]->SetFillColorAlpha( kRed+4, 0.40 );
+        errors2[i]->SetFillColorAlpha( kCyan, 0.20 );
         
       }
       
@@ -2883,7 +2883,7 @@ namespace jetHadron {
       
       if ( i == 0 ) {
         sys2[0]->SetFillStyle(1001);
-        sys2[0]->SetFillColorAlpha( kRed+4, 0.40 );
+        sys2[0]->SetFillColorAlpha( kCyan, 0.20 );
       }
       
     }
