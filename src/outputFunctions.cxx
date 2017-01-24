8aeee008b77651b3a06a811ab6f0bb1616170b41
nick-elsey <nicholas.elsey@gmail.com>
2017-01-23 21:59:05 -0500

    testing

--- src/outputFunctions.cxx
+++ src/outputFunctions.cxx
@@ -2672,7 +2672,7 @@ namespace jetHadron {
 
       
     TCanvas c1;
-    c1.SetLeftMargin( 0.2 );
+    c1.SetLeftMargin( 0.12 );
     for ( int i = 0; i < histograms[0].size(); ++i ) {
       
       double min, max;
@@ -2777,7 +2777,7 @@ namespace jetHadron {
       
       
       TCanvas c1;
-    c1.SetLeftMargin( 0.2 );
+    c1.SetLeftMargin( 0.12 );
     for ( int i = 0; i < histograms[0].size(); ++i ) {
       
       double min, max;
