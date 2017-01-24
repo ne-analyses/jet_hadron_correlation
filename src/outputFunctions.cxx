102120916c953dae219a0b91879ac1f9eae75a31
nick-elsey <nicholas.elsey@gmail.com>
2017-01-23 20:55:24 -0500

    testing

--- src/outputFunctions.cxx
+++ src/outputFunctions.cxx
@@ -2941,7 +2941,7 @@ namespace jetHadron {
     
     
     TCanvas c1;
-    c1.SetBottomMargin(0.2);
+    c1.SetBottomMargin(0.15);
     //c1.DrawFrame(-2, 20, -2, 20);
     for ( int i = 0; i < 2; ++i ) {
       
