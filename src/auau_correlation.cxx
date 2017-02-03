30be090d01522bd4b0c3107d45bbfe6b6e5694cf
nick-elsey <nicholas.elsey@gmail.com>
2017-02-03 00:12:49 -0500

    testing

--- src/auau_correlation.cxx
+++ src/auau_correlation.cxx
@@ -461,7 +461,8 @@ int main ( int argc, const char** argv ) {
         bkgdEstimator.set_particles( lowPtCons );
         // Subtract A*rho from the original pT
         fastjet::Subtractor bkgdSubtractor ( &bkgdEstimator );
-        LoResult = fastjet::sorted_by_pt( bkgdSubtractor( ClusterSequenceLow.inclusive_jets() ) );
+        //LoResult = fastjet::sorted_by_pt( bkgdSubtractor( ClusterSequenceLow.inclusive_jets() ) );
+        LoResult = fastjet::sorted_by_pt( ClusterSequenceLow.inclusive_jets() );
       }
       
       // Get the jets used for correlations
