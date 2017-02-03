58b442b429ad1b81c5f957a90ceec7271f6f8a19
nick-elsey <nicholas.elsey@gmail.com>
2017-02-03 04:14:10 -0500

    testing

--- src/auau_correlation.cxx
+++ src/auau_correlation.cxx
@@ -508,6 +508,10 @@ int main ( int argc, const char** argv ) {
       if ( analysisJets.size() == 0  )		{ continue; }
       nMatchedHard++;
       
+      if ( analysisJets[0].pt() < analysisJets[1].pt() ) {
+        std::cout<<"WE GOT ONE!"<<std::endl;
+      }
+      
       // now we have analysis jets, write the trees
       // for future event mixing
       vertexZBin = VzBin;
