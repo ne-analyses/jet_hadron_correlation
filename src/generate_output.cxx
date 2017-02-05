f4490581ec7dc7d86a7c759b4aa25f8dff6b1e36
nick-elsey <nicholas.elsey@gmail.com>
2017-02-04 23:14:25 -0600

    testing

--- src/generate_output.cxx
+++ src/generate_output.cxx
@@ -92,7 +92,7 @@ int main( int argc, const char** argv) {
   gStyle->SetOptTitle(1);
   gStyle->SetLegendBorderSize(0);
   gStyle->SetHatchesSpacing( 1.1 );
-  gStyle->SetHatchesLineWidth(1.5);
+  gStyle->SetHatchesLineWidth(2);
 
   
   // First check to make sure we're located properly
