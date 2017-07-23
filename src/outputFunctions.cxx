5bcfce11d79ae76f0cb473a52496bc7a5d32f249
nickelsey <nicholas.elsey@gmail.com>
2017-07-22 20:49:08 -0400

    testing errors...

--- src/outputFunctions.cxx
+++ src/outputFunctions.cxx
@@ -3329,6 +3329,7 @@ namespace jetHadron {
       if ( use_hist2 ) graph2 = GetErrorsFromHistogram( hist2[i], rangeLow, rangeHigh );
       
       TCanvas c1;
+      double ymin, ymax;
       
       graph1->GetXaxis()->SetTitle( xAxis_label.c_str() );
       graph1->GetYaxis()->SetTitle( yAxis_label.c_str() );
@@ -3337,25 +3338,25 @@ namespace jetHadron {
       graph1->SetMarkerStyle( 21 );
       graph1->SetMarkerSize( 2 );
       graph1->SetMarkerColor( 1 );
+      ymin = graph1->GetMinimum() - 0.05;
+      ymax = graph1->GetMaximum() + 0.05;
       if ( graph2 ) {
         graph2->SetLineColor( 2 );
         graph2->SetLineWidth( 2 );
         graph2->SetMarkerStyle( 22 );
         graph2->SetMarkerSize( 2 );
         graph2->SetMarkerColor( 2 );
+        if ( graph2->GetMinimum() < graph1->GetMinimum() ) {
+          ymin = graph2->GetMinimum() - 0.05;
+        }
+        if ( graph2->GetMaximum() > graph1->GetMaximum() ) {
+          ymax = graph2->GetMaximum() + 0.05;
+        }
       }
+      graph1->GetYaxis()->SetRangeUser( ymin, ymax );
+      graph1->Draw("AP");
+      if ( graph2 ) graph2->Draw("P");
       
-      if ( graph2 ) {
-        if ( graph1->GetMaximum() > graph2->GetMaximum() ) {
-          graph1->Draw("AP");
-          graph2->Draw("P");
-        }
-        else {
-          graph2->Draw("AP");
-          graph1->Draw("P");
-        }
-      }
-      else { graph1->Draw("AP"); }
       TLegend* leg = new TLegend(  0.65, 0.75, 0.88, 0.88 );
       leg->SetHeader( selector.ptBinString[i].c_str() );
       leg->AddEntry( graph1, hist1_name.c_str(), "lp"  );
