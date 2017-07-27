3ae7cb47c22dec36008e45472511390688f7e517
nickelsey <nicholas.elsey@gmail.com>
2017-07-27 11:59:37 -0400

    testing reading in correlation histograms from systematics file

--- src/generate_output.cxx
+++ src/generate_output.cxx
@@ -584,7 +584,7 @@ int main( int argc, const char** argv) {
   //******************************************
   // Now we need to test the systematic errors
   //******************************************
-  TFile sysIn( "out/added/pp/trg6/sysv4.root", "READ");
+  TFile sysIn( "out/added/pp/trg6/sysv5.root", "READ");
   std::vector<std::vector<TH1F*> > deta_sys, deta_sys_sub, dphi_sys, dphi_sys_sub;
   deta_sys.resize( 1 );
   deta_sys_sub.resize( 1 );
