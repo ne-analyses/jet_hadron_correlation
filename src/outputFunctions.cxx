7b2a15e8b9e639983d9ceb05cb6cd4f341b4a2d3
nickelsey <nicholas.elsey@gmail.com>
2017-09-05 02:43:19 -0400

    adding output script with only parts relevant to the analysis

--- src/outputFunctions.cxx
+++ src/outputFunctions.cxx
@@ -1169,7 +1169,7 @@ namespace jetHadron {
         tmpFit->SetParameter( 0, histograms[i][j]->GetBinContent( histograms[i][j]->GetMinimumBin() ) );
         tmpFit->SetParameter( 1, 1 );
         tmpFit->FixParameter( 2, 0 );
-        tmpFit->SetParameter( 3, 0.46 );
+        tmpFit->SetParameter( 3, 0.475 );
         
         histograms[i][j]->Fit( tmp.c_str(), "RMI" );
         std::string tmpSubName = "sub_" + tmp;
