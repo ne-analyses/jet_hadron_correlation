6aa2ad8e7126aac00d75435f134317ee23234006
nickelsey <nicholas.elsey@gmail.com>
2017-10-16 14:27:38 -0400

    testing

--- src/outputFunctions.cxx
+++ src/outputFunctions.cxx
@@ -1939,6 +1939,7 @@ namespace jetHadron {
       histograms[i]->GetYaxis()->SetTitleSize( 0.093 );
       histograms[i]->GetYaxis()->SetTitleOffset( 1.35 );
       histograms[i]->GetYaxis()->CenterTitle( true );
+      histograms[i]->GetYaxis()->SetRangeUser(-TMath::Pi()/2.0, TMath::Pi()*3.0/2.0 );
       histograms[i]->GetZaxis()->SetTitle("counts");
       histograms[i]->GetZaxis()->SetTitleSize( 0.05 );
       //histograms[i]->GetZaxis()->SetTitleOffset( 1.35 );
